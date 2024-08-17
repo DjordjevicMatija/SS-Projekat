b11b7c3dc6883374da085b75ffb840da56159aed
Matija Djordjevic <djordjevic.matija2@gmail.com>
2024-08-17 08:25:47 -0700

    Assembler.hpp function definitions

---
+++ inc/assembler.hpp
@@ -0,0 +1,43 @@
+using namespace std;
+
+void asmLabel();
+
+void asmGlobalDir();
+void asmExternDir();
+void asmSectionDir();
+void asmWordDir();
+void asmSkipDir();
+void asmEndDir();
+
+void asmHALT();
+void asmINT();
+void asmIRET();
+void asmCALL();
+void asmRET();
+
+void asmJMP();
+void asmBEQ();
+void asmBNE();
+void asmBGT();
+
+void asmPUSH();
+void asmPOP();
+
+void asmXCHG();
+
+void asmADD();
+void asmSUB();
+void asmMUL();
+void asmDIV();
+
+void asmNOT();
+void asmAND();
+void asmOR ();
+void asmXOR();
+void asmSHL();
+void asmSHR();
+
+void asmLD ();
+void asmST ();
+void asmCSRRD();
+void asmCSRWR();
